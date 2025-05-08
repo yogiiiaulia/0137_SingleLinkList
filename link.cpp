309fac71cb0cad1eff5cdf2d514bf6755f9b0a7e
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-05-08 16:03:56 +0700

    bool del

--- link.cpp
+++ link.cpp
@@ -72,4 +72,52 @@ public:
         return (*current != NULL);
     }
 
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous,  &current))
+            return false;
+
+        if (current == START) 
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
     
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah :\n";
+            currentNode = currentNode->next;
+        }
+        cout << endl
+    }
+}
+
+int main ()
+{
+    List mhs;
+    int nim;
+    char ch;
+    while(1)
+    {
+        cout
+            <<"Menu" << endl;
+            cout << endl
+                << "1. Menambah data kedalam list" << endl;
+            cout << "2. Menghapus data dari halaman list" <<endl;
+            cout << "Menampilkan data dalam list" << endl;
+            cout << "4. Mencari data dalam list" << endl;
+            cout << " 5. Keluar " << endl;
+            cout
+                << "Masukan pilihan (1-5): " << endl;
+                cin >> ch;
+    }
