baa90040c0bd6bbf7e3a7bfc2655000aac1cc840
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-05-08 16:47:15 +0700

    case 4

--- link.cpp
+++ link.cpp
@@ -155,5 +155,27 @@ int main ()
                 mhs.traverse();
             }
             break;
-
+            case '4' :
+            {
+                if (mhs.listEmpty() == true)
+                {
+                    cout << "\nList Kosong\n";
+                    break;
+                }
+                Node *previous, *current;
+                cout << endl
+                    << "Masukan no mahasiswa yang dicari : ";
+                    cin >> nim;
+                if (mhs.Search(nim, &previous, &current)== false)
+                    cout << endl
+                        << "Data tidak ditemukan" << endl;
+                else
+                {
+                    cout << endl
+                        << "Data ditemukan" << endl;
+                    cout << "\nNo Mahasiswa : " << current->noMhs << endl;
+                    cout << "\n";
+                }
+            }
+            break;
             
