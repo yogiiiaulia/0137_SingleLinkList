dbe4312885bac880441fbf5fc158e0ea35605cde
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-05-08 16:46:31 +0700

    case 2

--- link.cpp
+++ link.cpp
@@ -150,46 +150,4 @@ int main ()
                         << "Data dengan nomor mahasiswa" << nim << "berhasil dihapus" << endl;
             }
             break;
-            case '3' :
-            {
-                mhs.traverse();
-            }
-            break;
-
-            case '4' :
-            {
-                if (mhs.listEmpty() == true)
-                {
-                    cout << "\nList Kosong\n";
-                    break;
-                }
-                Node *previous, *current;
-                cout << endl
-                    << "Masukan no mahasiswa yang dicari : ";
-                    cin >> nim;
-                if (mhs.Search(nim, &previous, &current)== false)
-                    cout << endl
-                        << "Data tidak ditemukan" << endl;
-                else
-                {
-                    cout << endl
-                        << "Data ditemukan" << endl;
-                    cout << "\nNo Mahasiswa : " << current->noMhs << endl;
-                    cout << "\n";
-                }
-            }
-            break;
-            case '5' :
-            {
-
-            }
-            break;
-            default:
-            {
-                cout << "Pilihan salah!." << endl;
-            }
-            break;
-            }
-    } while (ch != 5);
-    
-}
+           
