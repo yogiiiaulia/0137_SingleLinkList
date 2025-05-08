1d111b69bd2b07a375ceae9e89255f8fc41a12b3
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-05-08 16:02:12 +0700

    void add node

--- link.cpp
+++ link.cpp
@@ -18,4 +18,22 @@ public:
         START = NULL;
     }
 
-   
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+        }
+
+        
