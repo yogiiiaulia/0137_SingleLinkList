79fadc8239b2fb22da8a32bb19ff9b2ee8fbc8ef
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-05-08 16:03:11 +0700

    bool search

--- link.cpp
+++ link.cpp
@@ -58,4 +58,18 @@ public:
         return (START == NULL);
     }
 
+    bool Search(int nim, Node **previous, Node **current)
+    {
+        *previous = *current;
+        *current = (*current)->next;
+
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+
+        return (*current != NULL);
+    }
+
     
